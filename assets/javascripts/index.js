81248441dcb15e75cb9fc2d9fbf6de08d6a778f1
darrickyong <darrickyong@gmail.com>
2020-05-20 14:50:16 -0700

    Added yr calculation

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,29 +1,43 @@
-// const width = document.getElementsByClassName("user-worth")[0];
+const nper = (rate, cmpd, pmt, pv, fv) => {
+  fv = parseFloat(fv);
+  pmt = pmt === "" ? 0 : parseFloat(pmt);
+  pv = pv === "" ? 0 : parseFloat(pv);
+  cmpd = parseFloat(cmpd);
+
+  if (!pv && !pmt) return 0;
+  
+  let yrs;
+  rate = eval(rate / (cmpd * 100));
+  if (rate == 0) {
+    yrs = -(fv + pv) / pmt;
+  } else {
+    yrs =
+      Math.log((-fv * rate + pmt) / (pmt + rate * pv)) / Math.log(1 + rate);
+  }
+  yrs = conv_number(yrs);
+  return yrs;
+};
+
+const conv_number = (expr) => {
+  if (expr === Infinity) return "FOREVER";
+  return parseFloat(expr.toFixed(2).toString()).toLocaleString("en");
+};
+
+const setYears = e => {
+  e.preventDefault();
+  let baseWorth = document.getElementsByClassName("user-worth")[0].value;
+  let baseSavings = document.getElementsByClassName("user-savings")[0].value;
+  let interest = document.getElementsByClassName("user-interest")[0].value;
+  let comparison =
+    netWorth[document.getElementsByClassName("user-selection")[0].selectedIndex]
+      .val;
+  let years = nper(interest, 1, baseSavings, baseWorth, -comparison);
+  document.getElementById("years").textContent = years;
+}
 
 document
   .getElementById("user-form")
-  .addEventListener("submit", e => {
-    e.preventDefault();
-    let baseWorth = document.getElementsByClassName("user-worth")[0].value;
-    let baseSavings = document.getElementsByClassName("user-savings")[0].value;
-    let comparison = netWorth[
-      document.getElementsByClassName("user-selection")[0].selectedIndex
-    ];
-    debugger
-  })
-
-document
-  .getElementsByClassName("slide-button")[0]
-  .addEventListener("click", (e) => {
-    document.getElementById("test").style.color = "red";
-  });
-
-document
-  .getElementsByClassName("zoom-button")[0]
-  .addEventListener("click", (e) => {
-    document.getElementById("test").style.color = "blue";
-  });
-
+  .addEventListener("submit", e => setYears(e))
 
 const netWorth = [
   { "name": "Jeff Bezos", "val": 145000000000, "img": "./assets/images/jeff.jpg" },
@@ -48,57 +62,47 @@ options.text( d => {
   })
 
 const svg = d3
-  .select(".main-right")
+  .select(".svg")
   .append("svg")
   .attr("width", "100%")
-  .attr("height", "100%")
   .style("background", "#F2F2F2");
   
-svg.append("text")
-  .attr("class", "this-is-you")
-  .attr("x", 50)
-  .attr("y", 90)
-  .text("This is you.")
-
-const compare = svg
-  .append("text")
-  .attr("class", "this-is-else")
-  .attr("x", 500)
-  .attr("y", 90)
-  .text(
-    `This is ${
-      netWorth[
-        document.getElementsByClassName("user-selection")[0].selectedIndex
-      ].name
-    }.`
-  );
-
-svg.append("rect")
-  .attr("x", 100)
-  .attr("y", 100)
-  .attr("width", "10px")
-  .attr("height", "10px")
+const you = d3.select(".this-is-you")
+  .append("svg")
+  .attr("width", 10)
+  .attr("height", 10)
+  .append("rect")
+  .attr("width", 10)
+  .attr("height", 10)
   .style("fill", "pink")
 
-const compareImg = svg
+const compareImg = d3.select(".this-is-else")
+  .append("svg")
+  .attr("width", 200)
+  .attr("height", 200)
   .append("image")
   .attr("width", 200)
   .attr("height", 200)
-  .attr("x", 500)
-  .attr("y", 100)
   .attr("xlink:href",
     netWorth[document.getElementsByClassName("user-selection")[0].selectedIndex]
       .img
   );
 
+const compareName = d3
+  .select("#compare-name")
+  .text(netWorth[document.getElementsByClassName("user-selection")[0].selectedIndex].name);
 
-  
+const compareVal = d3
+  .select("#compare-val")
+  .text(conv_number(netWorth[document.getElementsByClassName("user-selection")[0].selectedIndex].val));
 
 document
   .getElementsByClassName("user-selection")[0]
   .addEventListener("change", (e) => {
+    setYears(e);
     compareImg.attr("xlink:href", netWorth[e.target.selectedIndex].img);
-    compare.text(`This is ${ netWorth[e.target.selectedIndex].name }.`);
+    compareName.text(`${ netWorth[e.target.selectedIndex].name }`);
+    compareVal.text(`${ conv_number(netWorth[e.target.selectedIndex].val) }`);
   });
 
 
---
+++ assets/javascripts/tvm.js
@@ -0,0 +1,32 @@
+// Function to calculate total number of payments.
+
+const nper = (rate, per, pmt, pv, fv) => {
+  fv = parseFloat(fv);
+  pmt = parseFloat(pmt);
+  pv = parseFloat(pv);
+  per = parseFloat(per);
+
+  let nper_value;
+
+  rate = eval((rate) / (per * 100));
+  if (rate == 0) {
+    nper_value = -(fv + pv) / pmt;
+  } else {
+    nper_value = Math.log((-fv * rate + pmt) / (pmt + rate * pv)) / Math.log(1 + rate);
+  }
+  nper_value = conv_number(nper_value, 2);
+  return nper_value;
+}
+
+const conv_number = (expr, decplaces) => {
+  var str = "" + Math.round(eval(expr) * Math.pow(10, decplaces));
+  while (str.length <= decplaces) {
+    str = "0" + str;
+  }
+  var decpoint = str.length - decplaces;
+  return (str.substring(0, decpoint) + "." + str.substring(decpoint, str.length));
+}
+
+// console.log(nper(5, 1, 1000, 10000, -200000))
+
+module.exports.nper = nper;
