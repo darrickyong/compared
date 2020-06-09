d5b42aaefcf20f31802b4c6f25616a97d940b9ee
darrickyong <darrickyong@gmail.com>
2020-06-09 14:27:58 -0700

    Fix index.js file structure

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -32,6 +32,14 @@ const setYears = e => {
   let years = nper(growth, 1, baseContributions, baseSavings, -comparison);
   document.getElementById("years").textContent = years;
 }
+const width = 2000;
+const height = 550;
+const offset = 20;
+const groupSpacing = 0;
+const cellSpacing = 1;
+const cellSize = 8;
+const updateDuration = 125;
+const updateDelay = updateDuration / 500;
 
 const netWorth = [
   { "name": "Test", "val": 500000, "img":"./assets/images/test.jpeg" },
@@ -39,42 +47,6 @@ const netWorth = [
   { "name": "Boeing 777-300ER", "val": 375500000, "img": "./assets/images/boeing.jpg"},
   { "name": "Jeff Bezos", "val": 145000000000, "img": "./assets/images/jeff.jpg" },
 ]
-
-const you = d3
-  .select(".this-is-you")
-  .append("svg")
-  .attr("width", cellSize)
-  .attr("height", cellSize)
-  .append("rect")
-  .attr("width", cellSize)
-  .attr("height", cellSize)
-  .style("fill", "green");
-
-const compareImg = d3
-  .select(".this-is-else")
-  .append("svg")
-  .attr("width", "225px")
-  .attr("height", "150px")
-  .attr("class", "compare-img")
-  .append("image")
-  .attr("width", "225px")
-  .attr("height", "150px")
-  .attr(
-    "xlink:href",
-    netWorth[document.getElementsByClassName("user-selection")[0].selectedIndex]
-      .img
-  );
-
-const compareVal = d3
-  .select("#compare-val")
-  .text(
-    conv_number(
-      netWorth[
-        document.getElementsByClassName("user-selection")[0].selectedIndex
-      ].val
-    )
-  );
-
 // Select drop-down
 const selection = d3
   .select(".user-selection")
@@ -98,7 +70,38 @@ document
     compareImg.attr("xlink:href", netWorth[e.target.selectedIndex].img);
     compareVal.text(`${conv_number(netWorth[e.target.selectedIndex].val)}`);
   }); 
-  
+
+// Comparisons
+const you = d3
+  .select(".this-is-you")
+  .append("svg")
+  .attr("width", cellSize)
+  .attr("height", cellSize)
+  .append("rect")
+  .attr("width", cellSize)
+  .attr("height", cellSize)
+  .style("fill", "green");
+
+const compareImg = d3
+  .select(".this-is-else")
+  .append("svg")
+  .attr("width", "225px")
+  .attr("height", "150px")
+  .attr("class", "compare-img")
+  .append("image")
+  .attr("width", "225px")
+  .attr("height", "150px")
+  .attr(
+    "xlink:href",
+    netWorth[document.getElementsByClassName("user-selection")[0].selectedIndex].img
+  );
+
+const compareVal = d3
+  .select("#compare-val")
+  .text(
+    conv_number(netWorth[document.getElementsByClassName("user-selection")[0].selectedIndex].val)
+  );
+
 // Vizualize
 document
   .getElementsByClassName("user-form")[0]
@@ -107,7 +110,7 @@ document
     document.getElementsByClassName("blocks")[0].scrollLeft = 0;
     let baseline = document.getElementsByClassName("user-savings")[0].value;
     let compare = netWorth[document.getElementsByClassName("user-selection")[0].selectedIndex].val;
-
+    debugger
     if ((compare / baseline) > 40000) {
       if (window.confirm("It seems like there is a vast disparity, which will cause an usually long load time. Are you sure you want to continue?")) {
         update(compare / baseline);
@@ -118,15 +121,6 @@ document
   });
   
 // Block visualization
-const width = 2000;
-const height = 550;
-const offset = 20;
-const groupSpacing = 0;
-const cellSpacing = 1;
-const cellSize = 8;
-const updateDuration = 125;
-const updateDelay = updateDuration / 500;
-
 const svg = d3.select(".blocks").append("svg");
 
 let block = svg
